56395a02581d9523a2570fbd6ce8a99e38d3136d
Ors <38985578+Riku-S@users.noreply.github.com>
2022-01-29 14:20:08 +0200

    Making the triangle drawing function slightly more clean and testable (still broken af)

--- Assets/Scripts/AreaDrawer.cs
+++ Assets/Scripts/AreaDrawer.cs
@@ -10,14 +10,130 @@ public class AreaDrawer : MonoBehaviour
         public Vector2[] Vertices;
     }
 
+    private class RaycastPoint
+    {
+        public Vector2 Point;
+        public float Angle;
+        public RaycastPoint(Vector2 point, float angle)
+        {
+            Point = point;
+            Angle = angle;
+        }
+    }
+
     public PolygonInInspector[] Polygons;
     public GameObject TriangleDrawerObject;
+    private List<RaycastPoint> raycastPoints;
 
     private List<Vector2[]> lines_;
-    // Start is called before the first frame update
-    void Start()
+
+    const float Eps = 0.0001f;
+
+    private bool CompareVector2(Vector2 a, Vector2 b)
     {
+        return Mathf.Abs(a.y - b.y) + Mathf.Abs(a.x - b.x) < Eps;
+    }
+
+    private bool CompareFloat(float a, float b)
+    {
+        return Mathf.Abs(a - b) < Eps;
+    }
+
+    private class PointLine
+    {
+        public Vector2 Point;
+        public Vector2[] Line;
+
+        public PointLine(Vector2 point, Vector2[] line)
+        {
+            Point = point;
+            Line = line;
+        }
+    }
+
+    List<PointLine> RaycastAngle(List<Vector2[]> lines, RaycastPoint raycastPoint)
+    {
+        List<Vector2> points = new List<Vector2>();
+
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 direction = raycastPoint.Point - pos;
+        List<Vector2[]> intersected_lines = new List<Vector2[]>();
+
+        foreach (var line in lines)
+        {
+            Vector2 lineDirection = line[1] - line[0];
+            //Vector2 difference = line[0] - pos;
+
+            float posA = direction.y;
+            float posB = -direction.x;
+            float posC = posA * pos.x + posB * pos.y;
+
+            float lineA = lineDirection.y;
+            float lineB = -lineDirection.x;
+            float lineC = lineA * line[0].x + lineB * line[0].y;
+
+            float determinant = posA * lineB - lineA * posB;
+            if (determinant == 0)
+            {
+                continue;
+            }
+            Vector2 intersection = new Vector2(
+                (lineB * posC - posB * lineC) / determinant, 
+                (posA * lineC - lineA * posC) / determinant);
+
+            if (intersection.x > Mathf.Min(line[0].x, line[1].x) - Eps && 
+                intersection.x < Mathf.Max(line[0].x, line[1].x) + Eps && 
+                intersection.y > Mathf.Min(line[0].y, line[1].y) - Eps && 
+                intersection.y < Mathf.Max(line[0].y, line[1].y) + Eps && 
+                CompareFloat(Mathf.Atan2(direction.y, direction.x) , Mathf.Atan2(intersection.y - pos.y, intersection.x - pos.x)))
+            {
+                Debug.Log(intersection);
+                points.Add(intersection);
+                intersected_lines.Add(line);
+            }
+        }
+
+        points.Sort((Vector2 a, Vector2 b) => {
+            float lenA = (a - pos).sqrMagnitude;
+            float lenB = (b - pos).sqrMagnitude;
+            if (lenA == lenB)
+                return 0;
+            return lenA < lenB ? -1 : 1;
+        });
+
+        List<PointLine> finalPoints = new List<PointLine>();
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            foreach (var line in intersected_lines)
+            {
+                finalPoints.Add(new PointLine(points[i], line));
+                if (!CompareVector2(points[i], line[0]) && !CompareVector2(points[i], line[1]))
+                {
+                    return finalPoints;
+                }
+            }
+        }
+        return finalPoints;
+    }
+
+    void ClearTriangles()
+    {
+        TriangleDrawer[] objs = FindObjectsOfType<TriangleDrawer>();
+        foreach (var obj in objs)
+        {
+            Destroy(obj.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void DrawStuff()
+    {
+        ClearTriangles();
+
         lines_ = new List<Vector2[]>();
+        raycastPoints = new List<RaycastPoint>();
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
 
         foreach (var polygon in Polygons)
         {
@@ -26,6 +142,7 @@ public class AreaDrawer : MonoBehaviour
             {
                 lines_.Add(new Vector2[] { new Vector2(previous.x, previous.y), new Vector2(vertex.x, vertex.y) });
                 previous = vertex;
+                raycastPoints.Add(new RaycastPoint(vertex, Mathf.Atan2(vertex.y - pos.y, vertex.x - pos.x)));
             }
 
             // Draw the polygon
@@ -40,6 +157,50 @@ public class AreaDrawer : MonoBehaviour
             }
         }
 
+        List<List<PointLine>> endPoints = new List<List<PointLine>>();
+
+        foreach (var point in raycastPoints)
+        {
+            List<PointLine> newPoints = RaycastAngle(lines_, point);
+            endPoints.Add(newPoints);
+        }
+
+        raycastPoints.Sort((RaycastPoint a, RaycastPoint b) => {
+            if (a.Angle == b.Angle)
+                return 0;
+            return a.Angle < b.Angle ? -1 : 1;
+        });
+
+        //Debug.Log("End point rays: " + endPoints.Count);
+        for (int i = 0; i < endPoints.Count; i++)
+        {
+            var points = endPoints[i];
+            var nextPoints = endPoints[(i + 1) % endPoints.Count];
+
+            //Debug.Log("Points on ray: " + points.Count + ", next: " + nextPoints.Count);
+            foreach (var point in points)
+            {
+                foreach (var nextPoint in nextPoints)
+                {
+                    //Debug.Log(Mathf.Atan2(point.Point.y - pos.y, point.Point.x - pos.x));
+                    if (point.Line[0].x == nextPoint.Line[0].x && point.Line[0].y == nextPoint.Line[0].y && point.Line[1].x == nextPoint.Line[1].x && point.Line[1].y == nextPoint.Line[1].y)
+                    {
+                        if (CompareFloat(Mathf.Atan2(point.Point.y - pos.y, point.Point.x - pos.x), Mathf.Atan2(nextPoint.Point.y - pos.y, nextPoint.Point.x - pos.x)))
+                        {
+                            continue;
+                        }
+                        GameObject triangleObject = Instantiate(TriangleDrawerObject);
+                        TriangleDrawer drawer;
+                        if (triangleObject.TryGetComponent<TriangleDrawer>(out drawer))
+                        {
+                            drawer.DrawTriangle(pos, new Vector2(point.Point.x, point.Point.y), new Vector2(nextPoint.Point.x, nextPoint.Point.y), new Color(0.01f * i, 0, 0, 0.5f));
+                        }
+                    }
+                }
+            }
+        }
+
+        /*
         foreach(var line in lines_)
         {
             GameObject triangleObject = Instantiate(TriangleDrawerObject);
@@ -49,5 +210,14 @@ public class AreaDrawer : MonoBehaviour
                 drawer.DrawTriangle(new Vector2(0, 0), new Vector2(line[0].x, line[0].y), new Vector2(line[1].x, line[1].y), new Color(1, 0, 0, 0.5f));
             }
         }
+        */
+    }
+
+    private void Update()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position = transform.position + dir * Time.deltaTime * 3;
+
+        DrawStuff();
     }
 }
