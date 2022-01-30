bd255cdb5a8a473d26163406184669a5d198d4cf
Ors <38985578+Riku-S@users.noreply.github.com>
2022-01-30 12:46:55 +0200

    Fix lights being shown by dark player

--- Assets/Scripts/LightSourceController.cs
+++ Assets/Scripts/LightSourceController.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class LightSourceController : MonoBehaviour
 {
     public EmitLight[] LightSources;
+    private PlayerController playerController;
 
+
+    private void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
     // Update is called once per frame
     void Update()
     {
-        GlobalLightMesh.Clear();
-        foreach (var lightSource in LightSources)
+        if (playerController.playerType == PlayerController.PlayerType.Light)
         {
-            lightSource.DrawStuff();
+            GlobalLightMesh.Clear();
+
+            foreach (var lightSource in LightSources)
+            {
+                lightSource.DrawStuff();
+            }
+            GameObject bgSquare = GameObject.Find("Background Square");
+            Material mat = bgSquare.GetComponent<SpriteRenderer>().material;
+            GlobalLightMesh.ApplyToShader(mat);
         }
-        GameObject bgSquare = GameObject.Find("Background Square");
-        Material mat = bgSquare.GetComponent<SpriteRenderer>().material;
-        GlobalLightMesh.ApplyToShader(mat);
     }
 }
